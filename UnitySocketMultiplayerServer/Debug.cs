dfc1f826f76b1b27793a79b56a804bd027351f9f
jamabaiz <bartula.jan@gmail.com>
2020-11-06 13:55:16 +0100

    Console wrting tool

---
+++ UnitySocketMultiplayerServer/Debug.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitySocketMultiplayerServer
+{
+    static class Debug
+    {
+        private static bool silentMode;
+
+        public static void SetSilent(bool silentEnabled)
+        {
+            silentMode = silentEnabled;
+        }
+
+        public static void Log(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message);
+        }
+
+        public static void Log(string tag, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine('[' + tag + "]: " + message);
+        }
+        public static void LogColor(string tag, string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine('[' + tag + "]: " + message);
+        }
+
+        public static void LogInfo(string message)
+        {
+            LogColor("INFO", message, ConsoleColor.Green);
+        }
+        public static void LogData(string message)
+        {
+            if (!silentMode) LogColor("DATA", message, ConsoleColor.Blue);
+        }
+
+        public static void LogError(string message)
+        {
+            LogColor("ERROR", message, ConsoleColor.Red);
+        }
+
+    }
+}
