6e72b189937b9df500b5095e3d8d80986b4c2a97
jamabaiz <bartula.jan@gmail.com>
2020-11-06 11:12:17 +0100

    Add project files.

---
+++ UnitySocketMultiplayerServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitySocketMultiplayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
