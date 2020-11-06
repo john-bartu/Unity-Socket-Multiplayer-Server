52bb64903f65eb35350b3eb809b13d59a5506214
jamabaiz <bartula.jan@gmail.com>
2020-11-06 13:55:57 +0100

    Separate server accept loop and clients controll class

---
+++ UnitySocketMultiplayerServer/ClientController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UnitySocketMultiplayerServer
+{
+    class ClientController
+    {
+        static bool isRun=true;
+        static Dictionary<Guid, Client> clientMap = new Dictionary<Guid, Client>();
+
+        public static void AcceptClient(TcpClient clientSocket)
+        {
+            Guid uid = Guid.NewGuid();
+            Client newclient = new Client(clientSocket, uid);
+            clientMap.Add(uid, newclient);
+            Thread InstanceCaller = new Thread(new ThreadStart(newclient.Update));
+            InstanceCaller.Start();
+        }
+
+        public static void FreeClient(Client client)
+        {
+            Statistics.GetStat(client.GetUID()).PrintStat();
+            clientMap.Remove(client.GetUID());
+        }
+
+        public static void onServerStart()
+        {
+            // Start InputHandler for console commands
+            Thread InstanceCaller = new Thread(new ThreadStart(InputHandler.Update));
+            InstanceCaller.Start();
+        }
+
+        public static void stopServer()
+        {
+            isRun = false;
+            onServerClose();
+        }
+
+        public static void onServerClose()
+        {
+            Statistics.PrintStatistics();
+        }
+
+        public static bool isRunning()
+        {
+            return isRun;
+        }
+
+    }
+}
--- UnitySocketMultiplayerServer/Program.cs
+++ UnitySocketMultiplayerServer/Program.cs
@@ -6,80 +6,24 @@ using System.Threading;
 
 namespace UnitySocketMultiplayerServer
 {
-    class Client
-    {
-        TcpClient client;
-        NetworkStream stream;
-        StreamReader sr;
-        StreamWriter sw;
-
-
-        public Client(TcpClient newClient)
-        {
-            client = newClient;
-            stream = client.GetStream();
-            sr = new StreamReader(stream);
-            sw = new StreamWriter(stream);
-            Console.WriteLine("Client Stream Tunel created");
-        }
-
-        public void Update()
-        {
-            Console.WriteLine("Thread Started");
-            try
-            {
-                while (true)
-                {
-                    /*
-                      byte[] buffer = new byte[1024];
-                      stream.Read(buffer, 0, buffer.Length);
-                      int recv = 0;
-                      foreach (byte b in buffer)
-                      {
-                          if (b != 0)
-                          {
-                              recv++;
-                          }
-                      }
-                      string request = Encoding.UTF8.GetString(buffer, 0, recv);*/
-
-                    string received = sr.ReadLine();
-                    Console.WriteLine("CLIENT->SRV: " + received);
-
-
-                    string response = "You rock!";
-                    sw.WriteLine(response);
-                    sw.Flush();
-                    Console.WriteLine("SRV->Client: " + response);
-                }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Something went wrong.");
-                sw.WriteLine(e.ToString());
-            }
-
-            Console.WriteLine("Thread Ended");
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 1302);
             listener.Start();
-            while (true)
+
+            ClientController.onServerStart();
+
+            while (ClientController.isRunning())
             {
-                Console.WriteLine("Waiting for a connection.");
-                TcpClient client = listener.AcceptTcpClient();
-                Console.WriteLine("Client accepted.");
+                Debug.LogInfo("Waiting for a connection.");
+                TcpClient clientSocket = listener.AcceptTcpClient();
+                Debug.LogInfo("Client accepted.");
+                ClientController.AcceptClient(clientSocket);
 
-                Client newClient = new Client(client);
-
-                Thread InstanceCaller = new Thread(new ThreadStart(newClient.Update));
-                InstanceCaller.Start();
+                //Get some time for the server for another connection
+                Thread.Sleep(1000);
             }
         }
     }
